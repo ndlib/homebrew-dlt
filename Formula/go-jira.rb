ed513884f22dc207d6c7ac6e754643a2a15c4c91
Don Brower <dbrower@nd.edu>
2016-08-24 15:07:52 -0400

    Add (temporary) patches to go-jira
    Working on DLTP-563.

--- Formula/go-jira.rb
+++ Formula/go-jira.rb
@@ -4,6 +4,11 @@ class GoJira < Formula
   url "https://github.com/Netflix-Skunkworks/go-jira/archive/v0.1.6.tar.gz"
   version "0.1.6"
   sha256 "df935a14142bb08b1fec206964cdbec883955f48213a9865237c6bbe85b26338"
+  revision 2
+  # send the password prompt to stderr. do this until upstream either sends the prompt to stderr
+  # or until it supports putting the password in a file.
+  # this helps us integrate the output of the tool with other tools.
+  patch :DATA
 
   depends_on "go" => :build
 
@@ -15,3 +20,28 @@ class GoJira < Formula
     system "#{bin}/jira"
   end
 end
+
+__END__
+diff --git a/commands.go b/commands.go
+index 684949c..c1f3c5e 100644
+--- a/commands.go
++++ b/commands.go
+@@ -21,7 +21,7 @@ func (c *Cli) CmdLogin() error {
+ 		req, _ := http.NewRequest("GET", uri, nil)
+ 		user, _ := c.opts["user"].(string)
+ 
+-		fmt.Printf("Jira Password [%s]: ", user)
++		fmt.Fprintf(os.Stderr, "Jira Password [%s]: ", user)
+ 		pw, err := gopass.GetPasswdMasked()
+ 		if err != nil {
+ 			return err
+@@ -666,6 +666,9 @@ func (c *Cli) CmdTransition(issue string, trans string) error {
+ 			transName = name
+ 			transID = id
+ 			transMeta = transition.(map[string]interface{})
++			if strings.ToLower(name) == strings.ToLower(trans) {
++				break
++			}
+ 		}
+ 	}
+ 	if transID == "" {
