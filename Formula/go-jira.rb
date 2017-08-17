8ab8d2a5acf204102af1302af524eb2705bf7179
Don Brower <dbrower@nd.edu>
2017-08-17 13:20:17 -0400

    update go-jira to 0.1.14

--- Formula/go-jira.rb
+++ Formula/go-jira.rb
@@ -1,9 +1,9 @@
 class GoJira < Formula
   desc "Command line client for JIRA."
   homepage "https://github.com/Netflix-Skunkworks/go-jira"
-  url "https://github.com/Netflix-Skunkworks/go-jira/archive/v0.1.7.tar.gz"
-  version "0.1.7"
-  sha256 "82edcfd74b6ef2a187d2ebc3c1f95097db188313212480e40abaf5f4486efbd4"
+  url "https://github.com/Netflix-Skunkworks/go-jira/archive/v0.1.14.tar.gz"
+  version "0.1.14"
+  sha256 "561f388a0dcb6da531469fa913f9e5377f30f406b2afab78f6a0dd66b5352384"
   # send the password prompt to stderr. do this until upstream either sends the
   # prompt to stderr or until it supports putting the password in a file. this
   # helps us integrate the output of the tool with other tools.
@@ -21,16 +21,37 @@ class GoJira < Formula
 end
 
 __END__
-diff --git a/commands.go b/commands.go
-index 684949c..c1f3c5e 100644
---- a/commands.go
-+++ b/commands.go
-@@ -21,7 +21,7 @@ func (c *Cli) CmdLogin() error {
- 		req, _ := http.NewRequest("GET", uri, nil)
- 		user, _ := c.opts["user"].(string)
+diff --git a/Makefile b/Makefile
+index 36b2f96..10de1f6 100644
+--- a/Makefile
++++ b/Makefile
+@@ -35,7 +35,7 @@ DIST=$(CWD)$(SEP)dist
+ GOBIN ?= $(CWD)
  
--		fmt.Printf("Jira Password [%s]: ", user)
-+		fmt.Fprintf(os.Stderr, "Jira Password [%s]: ", user)
- 		pw, err := gopass.GetPasswdMasked()
- 		if err != nil {
- 			return err
+ CURVER ?= $(patsubst v%,%,$(shell [ -d .git ] && git describe --abbrev=0 --tags || grep ^\#\# CHANGELOG.md | awk '{print $$2; exit}'))
+-LDFLAGS:=-X jira.VERSION=$(CURVER) -w
++LDFLAGS:=-X gopkg.in/Netflix-Skunkworks/go-jira.v0.VERSION=$(CURVER) -w
+ 
+ # use make DEBUG=1 and you can get a debuggable golang binary
+ # see https://github.com/mailgun/godebug
+diff --git a/password.go b/password.go
+index a0ea242..a5c9456 100644
+--- a/password.go
++++ b/password.go
+@@ -3,6 +3,7 @@ package jira
+ import (
+ 	"bytes"
+ 	"fmt"
++	"os"
+ 	"os/exec"
+ 	"strings"
+ 
+@@ -36,7 +37,7 @@ func (c *Cli) GetPass(user string) string {
+ 	if passwd != "" {
+ 		return passwd
+ 	}
+-	fmt.Printf("Jira Password [%s]: ", user)
++	fmt.Fprintf(os.Stderr, "Jira Password [%s]: ", user)
+ 	pw, err := gopass.GetPasswdMasked()
+ 	if err != nil {
+ 		return ""
