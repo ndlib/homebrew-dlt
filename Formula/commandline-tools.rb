1a5985174ab43eaddb45b66e8d911d9902042d18
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-30 16:46:44 -0400

    Bumping commandline tools to latest version

--- Formula/commandline-tools.rb
+++ Formula/commandline-tools.rb
@@ -1,9 +1,9 @@
 class CommandlineTools < Formula
   desc "Command line tools used by Notre Dame Libraries"
   homepage "https://github.com/ndlib/commandline-tools"
-  url "https://github.com/ndlib/commandline-tools/archive/v2017.2.tar.gz"
-  version "v2017.2"
-  sha256 "c1adb3ee24fdd224c6779797f3600d6a52fe0ad79e5faa2dc1288aa94308ba3e"
+  url "https://github.com/ndlib/commandline-tools/archive/v2017.3.tar.gz"
+  version "v2017.3"
+  sha256 "54276ca1a44b115f8535c8f15861e9ae557839750ca1f8f651ae406a23bc7f2f"
 
   def install
     system "make", "install", "INSTALL_PATH=#{bin}"
