42548eeb5ebf3bcf8ca5d0b9c0b37084b497d61d
Don Brower <dbrower@nd.edu>
2017-08-31 10:09:05 -0400

    Fix installation of command line tools
    It was using a make file that I had locally but never pushed! Remove
    use of makefile and install each binary explicitly.
    
    Also add dependency on git.

--- Formula/commandline-tools.rb
+++ Formula/commandline-tools.rb
@@ -5,7 +5,10 @@ class CommandlineTools < Formula
   version "v2017.3"
   sha256 "54276ca1a44b115f8535c8f15861e9ae557839750ca1f8f651ae406a23bc7f2f"
 
+  depends_on "git" => :run
+
   def install
-    system "make", "install", "INSTALL_PATH=#{bin}"
+    bin.install "bin/next-build-identifier.sh"
+    bin.install "bin/tag-build.sh"
   end
 end
