2ed05946202acfda1ad0688a96095ecff327059d
Justin Gondron <jgondron@nd.edu>
2017-09-14 15:27:41 -0400

    Fixing https.

--- Formula/commandline-tools.rb
+++ Formula/commandline-tools.rb
@@ -1,7 +1,7 @@
 class CommandlineTools < Formula
   desc "Command line tools used by Notre Dame Libraries"
   homepage "https://github.com/ndlib/commandline-tools"
-  url "http://github.com/ndlib/commandline-tools/archive/v2017.4.tar.gz"
+  url "https://github.com/ndlib/commandline-tools/archive/v2017.4.tar.gz"
   version "v2017.4"
   sha256 "c18bd517dca8e26e5c89b2a21e6679d4f1e1aedbc9b0f7468e9aeeb8eca9a33b"
 
