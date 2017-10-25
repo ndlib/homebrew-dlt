d392199b84f034f425607331a74d36e781d2613a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-10-25 11:24:26 -0400

    Updating command line tools for v2017.6

--- Formula/commandline-tools.rb
+++ Formula/commandline-tools.rb
@@ -1,10 +1,9 @@
 class CommandlineTools < Formula
   desc "Command line tools used by Notre Dame Libraries"
   homepage "https://github.com/ndlib/commandline-tools"
-  url "https://github.com/ndlib/commandline-tools/archive/v2017.4.tar.gz"
-  version "v2017.4"
-  sha256 "c18bd517dca8e26e5c89b2a21e6679d4f1e1aedbc9b0f7468e9aeeb8eca9a33b"
-
+  url "https://github.com/ndlib/commandline-tools/archive/v2017.6.tar.gz"
+  version "v2017.6"
+  sha256 "918226ae2ad38aaa600e87f7c28bdd2c1571af712bf81136029aa12539d518af"
   depends_on "git" => :run
   depends_on "screen" => :run
 
