3ece500542e035fef0cd2889bb38c46d2c7de783
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-11-27 15:21:14 -0500

    Bumping brew to include build-pull-request-message

--- Formula/commandline-tools.rb
+++ Formula/commandline-tools.rb
@@ -1,13 +1,14 @@
 class CommandlineTools < Formula
   desc "Command line tools used by Notre Dame Libraries"
   homepage "https://github.com/ndlib/commandline-tools"
-  url "https://github.com/ndlib/commandline-tools/archive/v2017.6.tar.gz"
-  version "v2017.6"
-  sha256 "918226ae2ad38aaa600e87f7c28bdd2c1571af712bf81136029aa12539d518af"
+  url "https://github.com/ndlib/commandline-tools/archive/v2017.7.tar.gz"
+  version "v2017.7"
+  sha256 "a9090fe1d008517eb1e65ed521f5b3a65d397c6835189cf7aba6f7bfa625ff3f"
   depends_on "git" => :run
   depends_on "screen" => :run
 
   def install
+    bin.install "bin/build-pull-request-message"
     bin.install "bin/next-build-identifier.sh"
     bin.install "bin/tag-build.sh"
     bin.install "bin/hyrax.services"
