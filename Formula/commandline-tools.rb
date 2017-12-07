54138ae9b9d713927aa8ab0e4122485d0f2d17c0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-12-07 15:17:45 -0500

    Adding search_in_bundle to command line tools

--- Formula/commandline-tools.rb
+++ Formula/commandline-tools.rb
@@ -1,16 +1,18 @@
 class CommandlineTools < Formula
   desc "Command line tools used by Notre Dame Libraries"
   homepage "https://github.com/ndlib/commandline-tools"
-  url "https://github.com/ndlib/commandline-tools/archive/v2017.9.tar.gz"
+  url "https://github.com/ndlib/commandline-tools/archive/v2017.10.tar.gz"
   version "v2017.9"
-  sha256 "a9b3a9c2304f1e0cf44838c96b57b5c1ff0b7e8935de1c89c7643e75b8df59d1"
+  sha256 "4e208c3852d606f2e0cdc8eb6fca651120d069d6a7f4faef8ed3d835fe275e1d"
   depends_on "git" => :run
   depends_on "screen" => :run
+  depends_on "ag" => :run
 
   def install
     bin.install "bin/build-pull-request-message"
     bin.install "bin/next-build-identifier.sh"
     bin.install "bin/tag-build.sh"
     bin.install "bin/hyrax.services"
+    bin.install "bin/search_in_bundle"
   end
 end
